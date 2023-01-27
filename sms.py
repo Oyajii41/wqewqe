1aff973fa7d4b408139bce990713093eccf30c47
Oyajii41 <53922889+Oyajii41@users.noreply.github.com>
2023-01-28 02:42:20 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,7 +2,7 @@
     def Big(self):
         try:
              url = "https://a.postscript.io/v2/back_in_stock/subscribe"
-             json = {"product_url":"https://shop.biggames.io/products/santa-monkey-plush","shop_id":"248578","variant_id":"40454491439187","phone_number":{self.phone}}
+             json = {"product_url":"https://shop.biggames.io/products/santa-monkey-plush","shop_id":"248578","variant_id":"40454491439187","phone_number":self.phone}
              r = requests.post(url, json=json)
              if r.status_code == 200:
                 print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> BigGames")
