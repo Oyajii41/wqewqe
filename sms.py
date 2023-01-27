250b63b5a19340481a3d8fe6e2ce00cdedf29caa
Oyajii41 <53922889+Oyajii41@users.noreply.github.com>
2023-01-28 02:50:01 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -14,10 +14,10 @@ class SendSms():
             self.mail = mail
         else:
             self.mail = ''.join(choice(ascii_lowercase) for i in range(19)) + "@gmail.com"
-  
-  #biggames
+
+#biggames
 def Big(self):
-    try:
+      try:
              url = "https://a.postscript.io/v2/back_in_stock/subscribe"
              json = {"product_url":"https://shop.biggames.io/products/santa-monkey-plush","shop_id":"248578","variant_id":"40454491439187","phone_number":self.phone}
              r = requests.post(url, json=json)
@@ -26,6 +26,6 @@ def Big(self):
                 self.adet += 1
              else:
                   raise
-    except:
+      except:
             print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> BigGames")
 
