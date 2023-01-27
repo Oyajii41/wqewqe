e73d7df86f4ec1c943273d5e6236e59845942eae
Oyajii41 <53922889+Oyajii41@users.noreply.github.com>
2023-01-28 02:53:14 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -14,18 +14,18 @@ class SendSms():
             self.mail = mail
         else:
             self.mail = ''.join(choice(ascii_lowercase) for i in range(19)) + "@gmail.com"
-
-#biggames
-def Big(self):
-      try:
-             url = "https://a.postscript.io/v2/back_in_stock/subscribe"
-             json = {"product_url":"https://shop.biggames.io/products/santa-monkey-plush","shop_id":"248578","variant_id":"40454491439187","phone_number":self.phone}
-             r = requests.post(url, json=json)
-             if r.status_code == 200:
-                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> BigGames")
-                self.adet += 1
-             else:
-                  raise
-      except:
-            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> BigGames")
+  
+          #biggames
+        def Big(self):
+            try:
+                     url = "https://a.postscript.io/v2/back_in_stock/subscribe"
+                     json = {"product_url":"https://shop.biggames.io/products/santa-monkey-plush","shop_id":"248578","variant_id":"40454491439187","phone_number":self.phone}
+                     r = requests.post(url, json=json)
+                     if r.status_code == 200:
+                        print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> BigGames")
+                        self.adet += 1
+                     else:
+                          raise
+            except:
+                    print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> BigGames")
 
